5de3f7804add15f2b0c6dbcd2b59a6f88c9574fb
Panteamkhh <pantea.mkh18@gmail.com>
2026-06-17 11:16:50 +0330

    Complete Stage 3: improve task view with status icons

--- core-python/03-todo-app/main.py
+++ core-python/03-todo-app/main.py
@@ -1,45 +1,64 @@
-
 # List to store tasks in memory
 tasks = []
+
 
 while True:
 
     # Display menu options
     print("\n1) Add task")
     print("2) View tasks")
-    print("3) Delete tasks")
+    print("3) Delete task")
     print("4) Exit")
 
     # Get user input
     user_choice = input("Enter your choice: ")
-    
-    for i, task in enumerate(tasks, start=1):
-        print(i, task)
 
     # Add a new task
     if user_choice == "1":
-        task = input("Enter new task: ")
-        tasks.append(task)
-        print("Task added!")
+        task = input("Enter your new task : ")
+        tasks.append({"task": task,"done": False})
+        print("Task added successfully ✔")
+
 
     # Show all tasks
     elif user_choice == "2":
         if len(tasks) == 0:
-            print("No tasks yet!")
+            print("No tasks yet !")
         else:
-            print("\nYour Tasks:")
-            for task in tasks:
-                print("-", task)
+            for i, task in enumerate(tasks, start=1):
+              # Check task status    
+                if task["done"] :
+                    status = "✔"
+                else:
+                    status = "❌"
+            print(f"{i}. {status} {task['task']}")
 
+    # Delete a task
     elif user_choice == "3":
-        user_input = input("Enter your task that u want to delete: ")
-        tasks.pop(index = user_input - 1)
+        if len(tasks) == 0:
+            print("No tasks to delete !")
+        else:
+            for i, task in enumerate(tasks, start=1):
+                print(i, task)
+
+            try:
+                user_input = int(input("Enter task number to delete: "))
+
+                # Validate index
+                if 1 <= user_input <= len(tasks):
+                    removed = tasks.pop(user_input - 1)
+                    print(f"Deleted: {removed} ")
+                else:
+                    print("Invalid task number ")
+
+            except:
+                print("Please enter a valid number !")
 
     # Exit program
     elif user_choice == "4":
+        print("Goodbye ")
         break
 
-
-    # Handle invalid input
+    # Invalid input
     else:
-        print("Invalid choice")
+        print("Invalid choice ")
